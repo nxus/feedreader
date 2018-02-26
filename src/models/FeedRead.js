229530eb3315512f403b3e1348f0b39400d6d43d
Luke Opperman <luke@opperman.net>
2018-02-26 16:05:46 -0500

    typo

--- src/models/FeedRead.js
+++ src/models/FeedRead.js
@@ -3,7 +3,7 @@ import {BaseModel} from 'nxus-storage'
 
 export default BaseModel.extend({
   identity: 'feedreader-feedread',
-  connection: 'default'
+  connection: 'default',
   attributes: {
     url: { type: 'string'},
     meta: { type: 'json'}
